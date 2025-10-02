77edb365170a7dd84500f5ecac967c722700142a
Arshia Shahidi <arshia.shahidi@gmail.com>
2025-10-02 19:23:48 +0300

    feat: add environments configuration with 'ng generate environments'

--- src/app/features/users/pages/users-list/users-list.component.ts
+++ src/app/features/users/pages/users-list/users-list.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users-list.component.html'
 })
 export class UsersComponent {
+  private apiUrl = environment.apiUrl;
+  url = signal(this.apiUrl);
+
   constructor(private router: Router) { }
 
   goHome(): void {
